5174b6b4c044c81828f71c52ceab8a56b2f0dce1
Kas Elvirov <kas.elvirov@gmail.com>
2023-03-04 21:18:07 +0300

    fix and error with rollup building

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,20 @@
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import commonjs from '@rollup/plugin-commonjs';
 
 const name = require('./package.json').main.replace(/\.js$/, '')
 
 const bundle = config => ({
   ...config,
-  input: 'src/index.ts',
-  external: id => !/^[./]/.test(id),
+  input: './src/index.ts',
 });
 
 export default [
   bundle({
-    plugins: [esbuild()],
+    plugins: [esbuild.default(), nodeResolve(), typescript(), json(), commonjs(), esbuild.minify()],
     output: [
       {
         file: `${name}.js`,
@@ -26,10 +29,10 @@ export default [
     ],
   }),
   bundle({
-    plugins: [dts()],
+    plugins: [dts.default()],
     output: {
       file: `${name}.d.ts`,
       format: 'es',
     },
   }),
-];
+]
